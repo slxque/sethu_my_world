c8966d315fb637e7ef4a3b09ba3a49dd3ed71812
slxque <slxquekhanya.m@gmail.com>
2026-05-15 00:26:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
 const CACHE_NAME = 'sethu-diary-v1';
+const ASSETS = [
+    '/',
+    'index.html',
+    'diary.html',
+    'diary-style.css',
+    'sticky.css',
+    'script.js',
+    'manifest.json',
+    'icon.png'
+];
 
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Service Worker: Caching Assets');
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
